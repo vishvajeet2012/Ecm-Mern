69d5c933f1d163c3cb815b3a766845b80fdc6f95
vishvajeet <djritikshukla@gmail.com>
2024-11-07 00:56:19 +0530

    create ui admindashborad

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Auth/Login'
 import EmpoyeeDashboard from './components/Dashbord/EmpoyeeDashboard'
+import AdminDashbord from './components/Dashbord/AdminDashbord'
 
 function App() {
 
@@ -12,7 +13,8 @@ function App() {
      <>
     {/* //  <Login/> */}
       
-<EmpoyeeDashboard/>
+{/* <EmpoyeeDashboard/> */}
+<AdminDashbord/>
     </>
   )
 }
