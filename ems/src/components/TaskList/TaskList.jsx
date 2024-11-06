6c3f35c25aaeba9054c0efc19c3e9991a5ecf1f8
vishvajeet <djritikshukla@gmail.com>
2024-11-06 23:40:06 +0530

    update

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -14,6 +14,87 @@ function TaskList() {
           <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
 
         </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>  
       </div>
     </>
   );
