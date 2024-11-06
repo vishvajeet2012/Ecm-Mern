d8575ff0c6f5616e8bc4cdef87f26b921aa6b021
vishvajeet <djritikshukla@gmail.com>
2024-11-07 01:11:48 +0530

    update

--- ems/src/components/Dashbord/AdminDashbord.jsx
+++ ems/src/components/Dashbord/AdminDashbord.jsx
@@ -1,35 +1,13 @@
 import Header from "../other/Header";
+import TaskFrom from "../other/TaskFrom";
 
 function AdminDashbord() {
   return (
     <>
       <div className="h-screen w-full p-10 ">
         <Header />
-
-        <div>
-          <from className="flex items-start justify-between " >
-            <div>
-              <h3>Task Title</h3>
-              <input type="text" placeholder="Make aUi " />{" "}
-            </div>
-            <div>
-              <h3>Description</h3>
-
-              <textarea name="" id="" rows=""></textarea>
-            </div>
-<div>
-            <h3>Date</h3>
-            <input type="date" />
-            </div>
-            <div>
-            <h3>Assign to</h3>
-            <input type="text" placeholder="employe" />
-            <h3>Category</h3> </div>
-            <div>
-            <input type="text" placeholder="desgin, dev etc" />
-            <button>Create Task</button> </div>
-          </from>
-        </div>
+<TaskFrom></TaskFrom>
+     
       </div>
     </>
   );
