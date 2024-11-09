c39cb5092155a61bf27a4cc74adc607ab202935d
vishvajeet <djritikshukla@gmail.com>
2024-11-09 23:45:35 +0530

    nothing just push

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -3,72 +3,29 @@ import CompleteTask from "./CompleteTask";
 import FailedTask from "./FailedTask";
 import NewTask from "./NewTask";
 
-function TaskList() {
+function TaskList({data}) {
   return (
     <>
       <div
         id="tasklist"
         className="p-10 gap-5 overflow-x-auto flex items-center justify-start flex-nowrap py-5 w-full mt-10 h-[50%] hide-scrollbar"
       >
-      <AcceptTask/>
-      <NewTask/>
-        <CompleteTask/>
-    <FailedTask/>
+     { 
+          data.tasks.map((value,index)=>{
+            if(value.active){
+              return  <AcceptTask/>
     
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+            }
+            if(value.newTask){
+              return   <NewTask/>
+      
+            }
+           
+          })
 
-        </div>
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+     }
 
-        </div>
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
-
-        </div>
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
-
-        </div>
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
-
-        </div>
-        <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
-        <div className="flex justify-between items-center">
-          <h3 className="bg-red-500 text-sm px-3 py-2 rounded">High</h3>
-          <h4 className=" text-sm">11/6/2024</h4>
-          </div>
-          <h2 className="mt-5 text-xl font-semibold  ">Make Youtube video</h2>
-          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
-
-        </div>  
+      
       </div>
     </>
   );
--- ems/src/components/other/TaskNumber.jsx
+++ ems/src/components/other/TaskNumber.jsx
@@ -1,9 +1,11 @@
-function TaskNumber() {
+function TaskNumber({data}) {
+  console.error(data.tasks.
+    newTask)
   return (
     <>
       <div className="flex mt-10 justify-between gap-5">
         <div className="py-6 px-6 rounded-xl w-[45%]  bg-blue-400">
-          <h2 className="text-2xl font-semibold">{}</h2>
+          <h2 className="text-2xl font-semibold">{data.tasks.newTask}</h2>
           <h3 className="text-xl font-medium">New task</h3>
         </div>
         <div className="py-6 px-6 rounded-xl w-[45%]  bg-red-400">
