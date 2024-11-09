97d52d0c34c3d30be311c26662d8b8d0fd1b54f8
vishvajeet <djritikshukla@gmail.com>
2024-11-09 23:33:19 +0530

    change ui

---
+++ ems/src/components/TaskList/FailedTask.jsx
@@ -0,0 +1,18 @@
+function FailedTask() {
+    return ( 
+        <>
+              <div className="flex-shrink-0 h-full w-[300px] p-5 bg-green-600 rounded-xl">
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green-300 text-sm px-3 py-2 rounded">High</h3>
+          <h4 className=" text-sm">11/6/2024</h4>
+          </div>
+          <h2 className="mt-5 text-xl font-semibold  ">failedTask</h2>
+          <p className="text-sm mt-2 ">this is vishu iam from indian iam 100 year old</p>
+
+        </div>
+
+        </>
+     );
+}
+
+export default FailedTask;
