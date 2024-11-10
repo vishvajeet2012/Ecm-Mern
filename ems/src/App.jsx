782d2db24243089d0bec2f1ac31546beb965a7dc
vishvajeet <djritikshukla@gmail.com>
2024-11-10 23:42:31 +0530

    update

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,6 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Login from "./components/Auth/Login";
 import EmpoyeeDashboard from "./components/Dashbord/EmpoyeeDashboard";
