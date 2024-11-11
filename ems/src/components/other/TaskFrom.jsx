cdcc7196840e32a3e14c845d4cce73068d8318dc
vishvajeet <djritikshukla@gmail.com>
2024-11-11 14:12:18 +0530

    mobile responsive

--- ems/src/components/other/TaskFrom.jsx
+++ ems/src/components/other/TaskFrom.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function TaskForm() {
   const [task, setTask] = useState("");
@@ -8,13 +8,8 @@ function TaskForm() {
   const [description, setDescription] = useState("");
   const [tasks, setTasks] = useState([]);
 
-  useEffect(()=>{
-    console.log(tasks)
-  },[tasks])
   function formHandle(e) {
     e.preventDefault();
-
-   // store all form data in array 
     const newTask = {
       task,
       date,
@@ -22,12 +17,7 @@ function TaskForm() {
       category,
       description,
     };
-
-   
     setTasks([...tasks, newTask]);
-
-
-   
     setTask("");
     setDate("");
     setAssignee("");
@@ -37,14 +27,14 @@ function TaskForm() {
 
   return (
     <div className="p-5 bg-[#1c1c1c] mt-5 rounded">
-      <form onSubmit={formHandle} className="flex flex-wrap w-full items-start justify-between">
-        <div className="w-1/2">
+      <form onSubmit={formHandle} className="flex flex-wrap items-start justify-between w-full gap-5">
+        <div className="w-full md:w-1/2">
           <div>
             <h3 className="text-sm text-gray-300 mb-0.5">Task Title</h3>
             <input
               value={task}
               onChange={(e) => setTask(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              className="text-sm py-1 px-2 w-full md:w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
               type="text"
               placeholder="Make a UI design"
             />
@@ -54,7 +44,7 @@ function TaskForm() {
             <input
               value={date}
               onChange={(e) => setDate(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              className="text-sm py-1 px-2 w-full md:w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
               type="date"
             />
           </div>
@@ -63,7 +53,7 @@ function TaskForm() {
             <input
               value={assignee}
               onChange={(e) => setAssignee(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              className="text-sm py-1 px-2 w-full md:w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
               type="text"
               placeholder="Employee name"
             />
@@ -73,19 +63,19 @@ function TaskForm() {
             <input
               value={category}
               onChange={(e) => setCategory(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
+              className="text-sm py-1 px-2 w-full md:w-4/5 rounded outline-none bg-transparent border border-gray-400 mb-4"
               type="text"
               placeholder="Design, Dev, etc"
             />
           </div>
         </div>
 
-        <div className="w-2/5 flex flex-col items-start">
+        <div className="w-full md:w-2/5 flex flex-col items-start">
           <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
           <textarea
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
+            className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border border-gray-400"
             placeholder="Task description"
           />
           <button className="bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full">
@@ -93,22 +83,6 @@ function TaskForm() {
           </button>
         </div>
       </form>
-
- 
-      {/* <div className="mt-5 text-gray-300">
-        <h3 className="text-lg mb-2">Tasks List</h3>
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index} className="mb-2 p-2 border border-gray-500 rounded">
-              <strong>Task:</strong> {task.task} <br />
-              <strong>Date:</strong> {task.date} <br />
-              <strong>Assignee:</strong> {task.assignee} <br />
-              <strong>Category:</strong> {task.category} <br />
-              <strong>Description:</strong> {task.description}
-            </li>
-          ))}
-        </ul>
-      </div> */}
     </div>
   );
 }
